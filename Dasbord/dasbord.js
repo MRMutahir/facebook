af4d170c08f1bdbf938ea8fb40ade7d7cd527421
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-07-19 19:11:10 +0500

    ab jo post kare wo email or name ajy

--- Dasbord/dasbord.js
+++ Dasbord/dasbord.js
@@ -31,7 +31,7 @@ let displayuserData = async(usercurrentID) => {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
+        // console.log("Document data:", docSnap.data());
         const { name, email, date, month, years } = docSnap.data();
         login_name.innerHTML = name
         login_email.innerHTML = email
@@ -48,76 +48,68 @@ let postmenu = document.getElementById('postmenu');
 let postBtn1 = document.getElementById('postBtn1');
 let postMain = document.getElementById('postMain');
 
-console.log(postmenu.value);
+// console.log(postmenu.value);
 
-postBtn1.addEventListener('click', async () => {
+postBtn1.addEventListener('click', async() => {
+    const docRef = doc(db, "usersigindata", isloggedinuser);
+    const docSnap = await getDoc(docRef);
 
-   
-
-    // let readData =  async() => {
-    //     const querySnapshot = await getDocs(collection(db, "postcontent"));
-    // querySnapshot.forEach((doc) => {
-    //   console.log(`${doc.id} =>` , doc.data());
-   
-
-    // });
-    // }
-
-
-
-
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+    }
+    const { name, email, date, month, years } = docSnap.data();
+    // current_name.innerHTML = name
+    // current_email.innerHTML = email
+    // current_birthday.innerHTML = `${date }  - ${month }- ${years }`
 
     let divPost = document.createElement('div');
-    
-
     const postcontent = `  <div class="post">
     <div class="item1">
         <img src="../Dasbord/dasbordimg/profile1.jpeg" height="50px" width="50px" style="border-radius: 50px;" alt="">
-        <div>name <br> <span>12-3-9</span></div>
+        <div>${name} <br> <span>${email}</span></div>
         <span id="icon" class="ml-3">Icon</span>
       </div>
       <div class="text"> ${postmenu.value}</div>
       <div class="imgpost"></div>
       <div class="like">
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
+        <div>${date}</div>
+        <div>${month}</div>
+        <div>${years}</div>
       </div>
      </div>`
-        
+
     divPost.innerHTML = postcontent;
     postMain.appendChild(divPost);
-    // const docRef = addDoc(collection(db, "postcontent"), {
-    //     posttext: postmenu.value,
-    //     date: new Date().toLocaleString()
-    // });
+
     try {
         const docRef = await addDoc(collection(db, "postcontent"), {
             posttext: postmenu.value,
             date: new Date().toLocaleString(),
-            userid: isloggedinuser
+            userid: isloggedinuser,
+            // currentUsername =
         });
-      
-        console.log("Document written with ID: ", docRef.id);
-        console.log(isloggedinuser);
-      } catch (e) {
+
+        // console.log("Document written with ID: ", docRef.id);
+        // console.log(isloggedinuser);
+    } catch (e) {
         console.error("Error adding document: ", e);
-      }
-    // console.log("Document  ", docRef);
-    // console.log("Document written with ID: ", docRef.id);
-    // readData( docRef.id)
+    }
+
     displaypost();
 });
 
 displaypost()
 
-async function displaypost(){
+async function displaypost() {
     const querySnapshot = await getDocs(collection(db, "postcontent"));
-querySnapshot.forEach((doc) => {
-    postMain.innerHTML += `  <div class="post">
+    querySnapshot.forEach((doc) => {
+        postMain.innerHTML += `<div class="post">
     <div class="item1">
         <img src="../Dasbord/dasbordimg/profile1.jpeg" height="50px" width="50px" style="border-radius: 50px;" alt="">
-        <div>name <br> <span>12-3-9</span></div>
+        <div><br> <span>12-3-9</span></div>
         <span id="icon" class="ml-3">Icon</span>
       </div>
       <div class="text"> ${doc.data().posttext}</div>
@@ -128,5 +120,5 @@ querySnapshot.forEach((doc) => {
         <div>hi</div>
       </div>
      </div>`
-})
+    })
 }
